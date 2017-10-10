992f98b2dee03a80a0c7f4a10ab62be834e73516
taichi imai <imai0916taiti@gmail.com>
2017-10-10 14:48:55 +0000

    twitter

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,11 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in, only: [:new, :create]
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
-    @tasks = Task.all.page(params[:page])
+    if logged_in?
+      @tasks = current_user.tasks.page(params[:page])
+    end
   end
 
   def show
@@ -13,7 +17,7 @@ class TasksController < ApplicationController
   end
   
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.new(task_params)
     
     if @task.save
       flash[:success] = "新規タスクを投稿ました。"
